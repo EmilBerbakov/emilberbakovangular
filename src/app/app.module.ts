b934afcb97e32b5e0f32f7018b3a0afc01c91712
Emil Berbakov <emberbakov@gmail.com>
2023-04-15 00:50:00 -0500

    beginnings of Material Design Overhaul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LibrarydbComponent } from './pages/librarydb/librarydb.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { ErrorComponent } from './pages/error/error.component';
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
 import { MylibraryComponent } from './pages/mylibrary/mylibrary.component';
 import { AboutmeComponent } from './pages/aboutme/aboutme.component';
 import { RegisterComponent } from './pages/register/register.component';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -29,7 +32,10 @@ import { RegisterComponent } from './pages/register/register.component';
     AppRoutingModule,
     NgbModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
